b8ad5c01b03103cc3a35017a44f12ce5af120af0
skal48 <skal48@naver.com>
2024-03-16 21:34:27 +0900

    2743번

---
+++ baekjoonArrangement/src/string/Ex2743.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex2743 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		System.out.println(st.nextToken().length());
+		
+
+	}
+
+}
